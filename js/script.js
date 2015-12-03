234497f3b49387273f20e7b7895511f28187380f
Raziyeh Bazargan <Raziyeh@Raziyehs-MacBook-Pro.local>
2015-12-03 12:58:44 -0800

    add  two new question with array and for loop using  - version 1.1

--- js/script.js
+++ js/script.js
@@ -3,10 +3,18 @@
 var answerCity;
 var answerSport;
 var answerColor;
+var answerBook;
+
+var guessNumber;
+var finished=false;
 //count the correct answer
 var countCorrectAnswer=0;
+
 //User NAme Variable : store the User Name
 var userName;
+var myNum = 250;
+var arrayOfWeather=['rainy','sunny','cloudy','breezy'];
+
 
 //get Username of user and change it to UpperCase
 userName = prompt('What is your name?').toUpperCase();
@@ -51,5 +59,71 @@ answerCity = prompt('Did I grow up in Washington state?').toUpperCase();
      alert('Sorry ' + userName + '!' + ' My Favorite Color is Green!');
    }
 
- //show user number of correct answers
- alert(userName + ' you got '+ countCorrectAnswer + ' correct answers out of 3 questions');
+   //anwser questions Four from User
+   answerBook = prompt(' Is HTML & CSS my favorite book ?').toUpperCase();;
+     if (answerColor=='YES' || answerColor=='Y')
+     {
+       countCorrectAnswer++;
+       alert('Well Done!, You got it right ' + userName + '!');
+     }
+     else
+     {
+       alert('Sorry ' + userName + '!' + ' My Favorite Book is HTML & CSS!');
+     }
+
+
+   //show user number of correct answers
+   alert(userName + ' you got '+ countCorrectAnswer + ' correct answers out of 3 questions');
+
+   while (!finished)
+   {
+      var userNumber = parseInt(prompt('I\'m thinking of a number between 100 and 300 , Guess it!'));
+      finished = checkNumber();
+   }
+
+
+
+   function checkNumber()
+   {
+     if(userNumber>0 && userNumber<100)
+     {
+       alert('oh No, your number is too Low!');
+       return false;
+     }
+     else if (userNumber > 100 && userNumber < 250)
+    {
+      alert('oh No, your number is Low!');
+       return false;
+     }
+     else if (userNumber > 250 && userNumber <= 350)
+    {
+      alert('oh No, your number is high!');
+       return false;
+     }
+     else if (userNumber > 350 && userNumber <= 500)
+    {
+      alert('oh No, your number is too high!');
+       return false;
+     }
+     else if (userNumber >= 500)
+     {
+        alert('oh No, your number is too too high!');
+        return false;
+      }
+      else if(userNumber === myNum)
+      {
+        return true;
+        alert('Well Done!');
+      }
+    }
+
+var correctweatheranswer = false;
+while (!correctweatheranswer)
+{
+  var userWeatherAnswer = prompt('Please guess my favorite Weather! you can have 3 guess!' + '\n' + 'Rainy,sunny,Cloudy,breezy').toLowerCase();
+  if(arrayOfWeather.indexOf(userWeatherAnswer)!== -1)
+  {
+    correctweatheranswer=true;
+    alert('Wel Done, You guess right!');
+  }
+}
