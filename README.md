6fad50b27fc2a932a093a68b37eb2f365aed3e51
Raziyeh Bazargan <Raziyeh@Raziyehs-MacBook-Pro.local>
2015-12-03 13:39:44 -0800

    New Guessing game with 2 new questions-Version 1.1

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # guessing-game
 guessing-game for 201 class demo
+
+
+##Author : Raziyeh Bazargan
+
+
+##Version : 1.1
+
+
+##Changes
+add two new questions , using a array, function, while and for loop
+
+##TA
+Actually I don't know joke  :D
+but I want to say all of you (Benton, Kris and scott) are amazing and kind.
--- js/script.js
+++ js/script.js
@@ -1,41 +1,49 @@
 
-//Define Variables : store 3 User's answers
+
+//User Name Variable : store the User Name
+var userName;
+
+//Define Variables : store 4 User's answers
 var answerCity;
 var answerSport;
 var answerColor;
 var answerBook;
 
-var guessNumber;
-var finished=false;
-//count the correct answer
+//count the correct answer of 4 first question
 var countCorrectAnswer=0;
 
-//User NAme Variable : store the User Name
-var userName;
+//Define Variables for question 5 : guess number by user
+var guessNumber;
+var finished = false;
+
+//Define Varibales for Guessing 5 : set a number and ask user to guess
 var myNum = 250;
+
+//Define Varibales for Guessing 6: set a array with answers and ask user to guess
 var arrayOfWeather=['rainy','sunny','cloudy','breezy'];
 
 
-//get Username of user and change it to UpperCase
+//Part 1 :get Username of user and change it to UpperCase
 userName = prompt('What is your name?').toUpperCase();
 console.log('the Name of User is :' + userName);
 
 //show the Appropriate messeage to user
 alert('Good to meet you ' + userName + '! ' + ' enjoy your doughnuts');
 
-//anwser questions one from User
+//Part 2 : anwser questions one from User
 answerCity = prompt('Did I grow up in Washington state?').toUpperCase();
-//anwser questions one from User
+
+//Ask  First question from User
  if (answerCity =='NO' || answerCity=='N')
  {
       countCorrectAnswer++;
       alert('Well Done!, You got it right ' + userName + '!');
  }
- else
- {
+ else {
    alert('Sorry ' + userName + '!' + ' I grew up in Tehran');
  }
- //anwser questions two from User
+
+ //Ask  Secound question from User
  answerSport = prompt("Is my favorite sport TRX?").toUpperCase();
    if (answerSport =='YES' || answerSport=='Y')
    {
@@ -47,7 +55,7 @@ answerCity = prompt('Did I grow up in Washington state?').toUpperCase();
    alert('Sorry ' + userName + '!' + ' My Favorite sport is TRX!');
    }
 
- //anwser questions three from User
+  //Ask  Third question from User
  answerColor = prompt(' Is my favorite color Green?').toUpperCase();;
    if (answerColor=='YES' || answerColor=='Y')
    {
@@ -59,7 +67,7 @@ answerCity = prompt('Did I grow up in Washington state?').toUpperCase();
      alert('Sorry ' + userName + '!' + ' My Favorite Color is Green!');
    }
 
-   //anwser questions Four from User
+   //Ask  Fourth question from User
    answerBook = prompt(' Is HTML & CSS my favorite book ?').toUpperCase();;
      if (answerColor=='YES' || answerColor=='Y')
      {
@@ -75,17 +83,20 @@ answerCity = prompt('Did I grow up in Washington state?').toUpperCase();
    //show user number of correct answers
    alert(userName + ' you got '+ countCorrectAnswer + ' correct answers out of 3 questions');
 
+
+//Part 3 : Guessing a number by user
    while (!finished)
    {
       var userNumber = parseInt(prompt('I\'m thinking of a number between 100 and 300 , Guess it!'));
+      //recall the function checkNumber
       finished = checkNumber();
    }
 
-
-
+// Define a function for checking the user's answer : Low,too Low, too too Low , High, too High , too too hight
+//return the function result to while loop
    function checkNumber()
    {
-     if(userNumber>0 && userNumber<100)
+     if(userNumber > 0 && userNumber < 100)
      {
        alert('oh No, your number is too Low!');
        return false;
@@ -117,12 +128,16 @@ answerCity = prompt('Did I grow up in Washington state?').toUpperCase();
       }
     }
 
+//Part 4: ask question from user by a array of anwsers
 var correctweatheranswer = false;
 while (!correctweatheranswer)
-{
-  var userWeatherAnswer = prompt('Please guess my favorite Weather! you can have 3 guess!' + '\n' + 'Rainy,sunny,Cloudy,breezy').toLowerCase();
-  if(arrayOfWeather.indexOf(userWeatherAnswer)!== -1)
-  {
+ {
+   //get the answer from user by a prompt and convert the answer to lowercase
+   var userWeatherAnswer = prompt('Please guess my favorite Weather! you can have 3 guess!' + '\n' + 'Rainy,sunny,Cloudy,breezy').toLowerCase();
+
+   //check that user's anwer exist is our array
+   if(arrayOfWeather.indexOf(userWeatherAnswer)!== -1)
+    {
     correctweatheranswer=true;
     alert('Wel Done, You guess right!');
   }
